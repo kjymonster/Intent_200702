a8c6ce72442aa3ce34e884bd4da26665af3489cd
김재영 <kjymonster@naver.com>
2020-07-02 19:57:32 +0900

    finish로 메인화면으로 복귀하는 화면 작성

--- app/src/main/java/kr/co/tjoeun/intent_200702/FirstActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_200702/FirstActivity.kt
@@ -1,11 +1,24 @@
 package kr.co.tjoeun.intent_200702
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_first.*
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        backToMainBtn.setOnClickListener {
+
+            //메인화면으로 복귀
+//             Intent로 복귀하면, 복귀가 아닌, 새 화면이 계속 쌓이는 코드임
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+
+            //복귀 : FirstActivity를 종료시켜야함.
+            finish()
+        }
     }
 }
