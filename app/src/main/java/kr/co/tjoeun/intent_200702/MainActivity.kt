dba85f6902b02940a0803040f4aa781a68b9f6a6
김재영 <kjymonster@naver.com>
2020-07-02 19:50:04 +0900

    Intent 활용한 화면 이동 체험

--- app/src/main/java/kr/co/tjoeun/intent_200702/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_200702/MainActivity.kt
@@ -1,11 +1,22 @@
 package kr.co.tjoeun.intent_200702
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToFirstBtn.setOnClickListener {
+
+            //Intent => 비행기로 여행하는 것과 유사  (인텐트로 다른 액티비티로 이동)
+
+            //Intent() = 자바에서 new Intent(); 와 동일 (객체화)
+            val myIntent = Intent(this, FirstActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
