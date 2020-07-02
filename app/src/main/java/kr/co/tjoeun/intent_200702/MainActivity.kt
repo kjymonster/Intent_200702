18d4efbb63e50f070b469c18f3601f0ef51e131b
김재영 <kjymonster@naver.com>
2020-07-02 21:14:56 +0900

    startActivityForResult 사용  - 요청을 구별하는 고유 코드값을 적어주어야함.

--- app/src/main/java/kr/co/tjoeun/intent_200702/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_200702/MainActivity.kt
@@ -16,6 +16,18 @@ class MainActivity : AppCompatActivity() {
             val inputMessage = messageEdt.text.toString()
 
 
+         // 세번째 액티비티로 닉네임 변경하러 가기
+            changeNickNameBtn.setOnClickListener {
+
+                // 다른 화면으로 이동 : Intent 활용
+
+                val myIntent = Intent(this, EditNickNameActivity::class.java)
+
+                //이동 출발 : startActivityForResult => 그냥 가는 것이(startActivity) 아닌 결과를 가지러 이동.
+                //어떤 결과를 얻으러 가는지를 고유 번호로 구별해줘야함. (한 화면에 여러가지는 경우)
+                startActivityForResult(myIntent, 1000)
+            }
+
             //입력한 고유번호 Int로 받기
             val inputNum = numberEdt.text.toString().toInt()
 
