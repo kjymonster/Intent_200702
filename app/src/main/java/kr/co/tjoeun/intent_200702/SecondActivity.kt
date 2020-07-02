380ee5c27a612844588f3b9d15d4f33d874f9837
김재영 <kjymonster@naver.com>
2020-07-02 20:36:26 +0900

    메시지를 받아서 전달

--- app/src/main/java/kr/co/tjoeun/intent_200702/SecondActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_200702/SecondActivity.kt
@@ -2,10 +2,20 @@ package kr.co.tjoeun.intent_200702
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        //들어온 데이터를 받아서 텍스트뷰에 반영
+
+
+        //액티비티가 가진 intent 변수를 통해서 첨부된 (String (이름표) ) 데이터 저장
+        val receivedMessage = intent.getStringExtra("message")
+
+        //저장한 데이터를 텍스트뷰에 반영
+        receivedMessageTxt.text = receivedMessage
     }
 }
