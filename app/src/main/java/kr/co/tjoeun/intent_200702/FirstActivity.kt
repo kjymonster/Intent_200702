411a1e43332c9201da0bbc0b7a5832a1dc1b86be
김재영 <kjymonster@naver.com>
2020-07-02 19:51:46 +0900

    Intent 활용한 화면 이동 체험

--- app/src/main/java/kr/co/tjoeun/intent_200702/FirstActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_200702/FirstActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_first2)
+        setContentView(R.layout.activity_first)
     }
 }
