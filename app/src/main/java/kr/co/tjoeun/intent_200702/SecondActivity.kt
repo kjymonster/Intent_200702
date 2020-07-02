e3fac2041627ada958ffa1e83379b1c25a0fdd5d
김재영 <kjymonster@naver.com>
2020-07-02 20:59:18 +0900

    받아주는 쪽에서 Int를 받아서 반영해봄.   -getIntExtra로 기본값 설정   -String으로 가공 문법 활용

--- app/src/main/java/kr/co/tjoeun/intent_200702/SecondActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_200702/SecondActivity.kt
@@ -18,6 +18,15 @@ class SecondActivity : AppCompatActivity() {
         //저장한 데이터를 텍스트뷰에 반영
         receivedMessageTxt.text = receivedMessage
 
+        //첨부된 Int 데이터를 변수에 저장
+        //만약 첨부가 안되었다면 -1을 대신 집어넣자.
+        //JAVA의 기본형 변수를 받을때만 필요한 작업 (기본 자료형에서만)
+        val recNum = intent.getIntExtra("number", -1)
+            //String은 아무것도 없으면 null이 기본이지만, int는 기본값을 직접 넣어줘야함
+
+        //받은 데이터 : 1234 등의 숫자.
+        //세팅할 내용은 => "고유번호 : 1234" 와 같이 가공된 String을 넣어야함. (JAVA의 String.format)
+        numberTxt.text = "고유번호 : ${recNum}"
 
     }
 }
